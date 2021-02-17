1bc9b660ab91e988361b639ca3e07636fc9a6094
Vincent Vlieghe <vincent.vlieghe@delen.be>
2021-02-17 19:45:51 +0000

    Added devcontainer and dockercompose stuff

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/virtrixke/golang-rabbitmq
+
+go 1.15
+
+require github.com/michaelklishin/rabbit-hole/v2 v2.6.0
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	rh "github.com/michaelklishin/rabbit-hole/v2"
+)
 
 func main() {
-	fmt.Println("Hello rabbit")
+	rmqc, err := rh.NewClient("http://rabbitmq:15672", "user", "password")
+
+	if err != nil {
+		log.Fatal("Cannot initialize: ", err)
+	}
+
+	resp, err := rmqc.Overview()
+
+	if err != nil {
+		log.Fatal("Cannot show overview: ", err)
+	}
+
+	fmt.Println(resp.RabbitMQVersion)
+
+	xs, err := rmqc.ListNodes()
+
+	if err != nil {
+		log.Fatal("Failed to list nodes: ", err)
+	}
+
+	fmt.Println(xs)
 }
